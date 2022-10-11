bfbd1e10d67ffd92a7d7dc5906af1e38da71988f
Chyavan Shenoy <32121448+ChyavanShenoy@users.noreply.github.com>
2022-10-12 00:11:29 +0530

    updated request

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
+from os import environ
 from pydantic import BaseModel, Field
 from fastapi import FastAPI, Body
 import utils.fileops as fileops
 import prediction_machine as pm
 from fastapi.middleware.cors import CORSMiddleware
 
-origins = [
-    "http://localhost",
-]
-
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-class ImgLoc(BaseModel):
-    # image_data: str = Field(description="Path to the image")
-    image_data: str
+# class ImgLoc(BaseModel):
+#     image_data: str = Field(description="Base64 encoded image")
 
-    # def __getitem__(self, item):
-    #     return getattr(self, item)
+#     # def __getitem__(self, item):
+#     #     return getattr(self, item)
 
 
 # on startup event delet files inside temp_files folder
@@ -39,8 +35,12 @@ def read_root():
 
 
 @app.post("/bill")
-def detect_bill(image_location: ImgLoc = Body(embed=True)):
-    image = fileops.base64_to_image(image_location.image_data)
+# def detect_bill(image_location: ImgLoc = Body(embed=True)):
+def detect_bill(image_data: str = Body(..., embed=True)):
+    print("-----------------")
+    print(image_data)
+    print("-----------------")
+    image = fileops.base64_to_image(image_data)
     file_name = fileops.write_image_to_file(image)
     result, synth_image = pm.predict(file_name)
     synth_image_base64 = fileops.image_to_base64(synth_image)
@@ -48,3 +48,13 @@ def detect_bill(image_location: ImgLoc = Body(embed=True)):
         "synth_image": synth_image_base64,
         "result": result,
     }
+
+
+def main():
+    import uvicorn
+    # use uvicorn to run the app at env:8000
+    uvicorn.run(app, host="localhost", port=environ.get("PORT", 8080))
+
+
+if __name__ == "__main__":
+    main()
